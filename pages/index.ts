a3c67ed1c1be0950365567d3b7df04e864664b70
Jared Pereira <jared@awarm.space>
2020-03-17 15:53:19 -0400

    use getServerSideProps in index.ts

--- pages/index.ts
+++ pages/index.ts
@@ -31,7 +31,7 @@ const Landing:NextPage<{courses:Course[]}> = (props) => {
   ])
 }
 
-export const getStaticProps = () => {
+export const getServerSideProps = () => {
   let courses = fs.readdirSync('./pages/courses').filter(file => {
     return !fs.lstatSync('./pages/courses/' + file).isDirectory()
   }).map(file => {
