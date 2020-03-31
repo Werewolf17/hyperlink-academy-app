758c2adc694b8dbfa7485d09519f34523cf410fe
Jared Pereira <jared@awarm.space>
2020-03-31 14:43:29 -0400

    add some images and copy

---
+++ components/TitleImg.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+let TitleImg = styled('img')`
+image-rendering: pixelated;
+image-rendering: -moz-crisp-edges;
+image-rendering: crisp-edges;
+display: block;
+margin: auto;
+`
+export default TitleImg
--- pages/signup.ts
+++ pages/signup.ts
@@ -5,18 +5,16 @@ import { NextPageContext } from 'next'
 import {Section} from '../components/Section'
 import {getToken} from '../src/token'
 import {Form, Label, Button, Input, Error} from '../components/Form'
+import TitleImg from '../components/TitleImg'
 
 const Login = () => {
-  let [email, setEmail] = useState('')
-  let [password, setPassword] = useState('')
-  let [confPassword, setConfPassword] = useState('')
+  let [formState, setFormState] = useState({email:'', password:'', confPassword:''})
   let [error, setError] = useState<'user exists' | null>(null)
-  let [state, setState] = useState<'normal' | 'loading' | 'success' | 'error'>('normal')
+  let [state, setState] = useState<'normal' | 'loading' | 'success'>('normal')
 
   useEffect(()=>{
     setError(null)
-  }, [email])
-
+  }, [formState.email])
 
   if(state === 'success') {
     return h(Section, [
@@ -25,55 +23,57 @@ it, check out your Spam folder.`
     ])
   }
 
-  return h(Section, {}, h(Form, {onSubmit: async (e) => {
-    e.preventDefault()
-    setState('loading')
-    let res = await (await fetch('/api/signup', {
-      method: "POST",
-      body: JSON.stringify({email, password})
-    })).json()
+  return h(Section, {}, [
+    h(TitleImg, {src: '/img/start_journey.png', width: '250px'}),
+    h(Form, {onSubmit: async (e) => {
+      e.preventDefault()
+      setState('loading')
+      let res = await (await fetch('/api/signup', {
+        method: "POST",
+        body: JSON.stringify({email:formState.email, password: formState.password})
+      })).json()
 
-    if(!res.success) {
-      setState('error')
-      setError('user exists')
-    }
-    else {
-      setState('success')
-    }
-  }}, [
-    h('p', `Welcome to hyperlink.academy!`),
-    error ? h(Error, error) : null,
-    h(Label, [
-      h(Input, {type: 'email',
-                placeholder: 'email',
-                required: true,
-                value: email,
-                onChange: (e)=> setEmail(e.currentTarget.value)})
-    ]),
-    h(Label, [
-      h(Input, {type: 'password', placeholder: 'password',
-                required: true,
-                minLength: 8,
-                value: password,
-                onChange: (e)=> setPassword(e.currentTarget.value)})
-    ]),
-    h(Label, [
-      h(Input, {type: 'password', placeholder: 'confirm password',
-                required: true,
-                 value: confPassword,
-                onChange: (e)=> {
-                  setConfPassword(e.currentTarget.value)
-                  if(e.currentTarget.value !== password) {
-                    e.currentTarget.setCustomValidity('passwords do not match')
+      if(!res.success) {
+        setError('user exists')
+      }
+      else {
+        setState('success')
+      }
+    }}, [
+      h('p', `Welcome to hyperlink.academy!`),
+      error ? h(Error, error) : null,
+      h(Label, [
+        h(Input, {type: 'email',
+                  placeholder: 'email',
+                  required: true,
+                  value: formState.email,
+                  onChange: (e)=> setFormState({...formState, email:e.currentTarget.value})})
+      ]),
+      h(Label, [
+        h(Input, {type: 'password', placeholder: 'password',
+                  required: true,
+                  minLength: 8,
+                  value: formState.password,
+                  onChange: (e)=> setFormState({...formState, password:e.currentTarget.value})})
+      ]),
+      h(Label, [
+        h(Input, {type: 'password', placeholder: 'confirm password',
+                  required: true,
+                  value: formState.confPassword,
+                  onChange: (e)=> {
+                    setFormState({...formState, confPassword:e.currentTarget.value})
+                    if(e.currentTarget.value !== formState.password) {
+                      e.currentTarget.setCustomValidity('passwords do not match')
+                    }
+                    else {
+                      e.currentTarget.setCustomValidity('')
+                    }
                   }
-                  else {
-                    e.currentTarget.setCustomValidity('')
-                  }
-                }
-               })
-    ]),
-    state === 'loading' ? h('div', 'Loading') : h(Button, {type: 'submit'}, 'login')
-  ]))
+                 })
+      ]),
+      state === 'loading' ? h('div', 'Loading') : h(Button, {type: 'submit'}, 'login')
+    ])
+  ])
 }
 
 export default Login
