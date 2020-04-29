38c78ff64d1a0ecd30bc89af999da8f9b2c67361
Jared Pereira <jared@awarm.space>
2020-04-28 20:15:00 -0400

    update nextjs

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/
 })
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'ts', 'md', 'mdx']
+  pageExtensions: ['js', 'jsx', 'ts', 'md', 'mdx'],
+  experimental: {
+    reactRefresh: true
+  }
 })
