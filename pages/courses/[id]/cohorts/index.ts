9d11c3ff5162426d02f66e520e61889aaaa98901
Jared Pereira <jared@awarm.space>
2020-07-22 17:24:16 -0400

    properly link to cohort page from enroll page

--- pages/courses/[id]/cohorts/index.ts
+++ pages/courses/[id]/cohorts/index.ts
@@ -112,7 +112,10 @@ let Cohort = (props: {
                 ])
             ]),
             h(Text, {source: props.details.text.slice(0, 400) + (props.details.text.length > 400 ?'...' : '')}),
-            h(Link, {href: '/courses/[id]/[cohortId]', as: `/courses/${props.course}/${props.id}`}, h('a', {style: {textDecoration: 'underline'}}, h('b', 'See more details')))
+            h(Link, {
+                href: '/courses/[id]/cohorts/[cohortId]',
+                as: `/courses/${props.course}/cohorts/${props.id.split('-').slice(-1)[0]}`
+            }, h('a', {style: {textDecoration: 'underline'}}, h('b', 'See more details')))
         ]),
         h(Box, {gap:8, style: {justifyContent: 'right', textAlign: 'right'}}, [
             h(Primary, {onClick, disabled: props.invite_only && !props.invited}, status === 'loading' ? h(Loader) : ' Join this Cohort'),
