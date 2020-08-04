652ac38cd1a0f42dbdbbcc2af111714cc91c3d77
Jared Pereira <jared@awarm.space>
2020-08-03 22:48:02 -0400

    add watching course notification email

--- emails/index.ts
+++ emails/index.ts
@@ -3,6 +3,8 @@ import cohortEnrollmentMetadata from './templates/cohort-enrollment/meta.json'
 import verifiyEmailMetadata from './templates/welcome/meta.json'
 import resetPasswordMetadata from './templates/password-reset/meta.json'
 import inviteToCourseMetadata from './templates/invite-to-course/meta.json'
+import enrollNotificationMetada from './templates/enroll-notfication/meta.json'
+import watchingNotificationMetadata from './templates/watching-notification/meta.json'
 
 var client = new postmark.ServerClient(process.env.POSTMARK_TOKEN || '');
 
@@ -10,6 +12,8 @@ export const sendResetPasswordEmail = sendEmail(resetPasswordMetadata)
 export const sendVerificationEmail = sendEmail(verifiyEmailMetadata)
 export const sendCohortEnrollmentEmail = sendEmail(cohortEnrollmentMetadata)
 export const sendInviteToCourseEmail = sendEmail(inviteToCourseMetadata)
+export const sendEnrollNotificationEmaill = sendEmail(enrollNotificationMetada)
+export const sendWatchingNotificationEmail = sendEmail(watchingNotificationMetadata)
 
 type EmailMetadata = {
   Alias: string,
