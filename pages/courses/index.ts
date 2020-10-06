8d493c1495cfff401242620756a2d4e6350aacb6
Jared Pereira <jared@awarm.space>
2020-10-06 15:20:27 -0400

    sort course list

--- pages/courses/index.ts
+++ pages/courses/index.ts
@@ -12,6 +12,14 @@ export default function Courses(props:Props) {
     h('h1', "The Course List"),
     h(FlexGrid, {min: 328, mobileMin: 200},
       courses?.courses
+        .sort((a, b)=>{
+          let upcomingCohortA = a.course_cohorts.filter(c=>new Date(c.start_date) > new Date())[0]
+          let upcomingCohortB = b.course_cohorts.filter(c=>new Date(c.start_date) > new Date())[0]
+          if(!upcomingCohortA && !upcomingCohortB) return a.name > b.name ? 1 : -1
+          if(!upcomingCohortA) return 1
+          if(!upcomingCohortB) return -1
+          return new Date(upcomingCohortA.start_date) < new Date(upcomingCohortB?.start_date) ? -1 : 1
+        })
         .map(course => {
           return h(CourseCard, course)
         }))
